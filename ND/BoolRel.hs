79f8582f2da194454caea2ce7f5b1c7b1aad6285
Eric Walkingshaw <walkiner@eecs.oregonstate.edu>
2010-08-23 14:10:46 -0700

    Added boolean relation module.
    Ignore-this: 3553a848a4aea721bb885d66d1673560
    
    darcs-hash:20100823211046-4d841-0bbd2ef9d2b3e6c9a6888ec8c43f4d28abaa1af2.gz

---
+++ ND/BoolRel.hs
@@ -0,0 +1,96 @@
+
+module ND.BoolRel where
+
+import Data.Function (on)
+import Data.Monoid
+import Data.List (intersperse,nub,sort)
+
+
+-----------------------
+-- Boolean Relations --
+-----------------------
+
+newtype Con a = Con [a] -- conjunction
+newtype Dis a = Dis [a] -- disjunction
+
+type DC a = Dis (Con a) -- disjunction of conjunctions
+
+-- logical relation (better name?)
+class (Functor r, Monad r) => Rel r where
+  toList   :: r a -> [a]
+  fromList :: [a] -> r a
+
+toList2 :: (Rel r, Rel s) => r (s a) -> [[a]]
+toList2 = toList . fmap toList
+
+fromList2 :: (Rel r, Rel s) => [[a]] -> r (s a)
+fromList2 = fromList . map fromList
+
+onList :: Rel r => ([a] -> [b]) -> r a -> r b
+onList f = fromList . f . toList
+
+onList2 :: (Rel r, Rel s) => ([[a]] -> [[b]]) -> r (s a) -> r (s b)
+onList2 f = fromList2 . f . toList2
+
+-- (set) equality of two expressions
+relEq :: (Rel r, Ord a) => r a -> r a -> Bool
+relEq = (==) `on` (sort . nub . toList)
+
+-- exploit associativity to remove a relation nesting
+assoc :: Rel r => r (r a) -> r a
+assoc = onList (concatMap toList)
+
+-- exploit distiributivity to swap the nesting of relations
+dist :: (Rel r, Rel s) => r (s a) -> s (r a)
+dist = fromList2 . dist' . toList2
+  where dist' []       = []
+        dist' [xs]     = [[y]  | y <- xs]
+        dist' (xs:xss) = [y:ys | y <- xs, ys <- dist' xss]
+
+-- flatten a nested DC expression
+flatten :: DC (DC a) -> DC a
+flatten = fmap assoc . assoc . fmap dist
+
+
+---------------
+-- Instances --
+---------------
+
+instance Rel Con where
+  toList (Con as) = as
+  fromList = Con
+instance Rel Dis where
+  toList (Dis as) = as
+  fromList = Dis
+
+instance Ord a => Eq (Con a) where (==) = relEq
+instance Ord a => Eq (Dis a) where (==) = relEq
+
+instance Functor Con where
+  fmap f (Con as) = Con (map f as)
+instance Functor Dis where
+  fmap f (Dis as) = Dis (map f as)
+
+instance Monad Con where
+  return a     = Con [a]
+  Con as >>= f = Con (concat [bs | Con bs <- map f as])
+  fail _       = Con []
+
+instance Monad Dis where
+  return a     = Dis [a]
+  Dis as >>= f = Dis (concat [bs | Dis bs <- map f as])
+  fail _       = Dis []
+
+parens :: String -> String
+parens s = "(" ++ s ++ ")"
+
+sepBy :: (Show a, Rel r) => String -> r a -> String
+sepBy s = concat . intersperse s . map show . toList
+
+-- These Show instances look nice for DC expressions, but could be confusing
+-- in other contexts.  Uncomment the parens functions for a more explicit
+-- representation.
+instance Show a => Show (Con a) where
+  show = {- parens . -} sepBy "^"
+instance Show a => Show (Dis a) where
+  show = {- parens . -} sepBy " v "
